1a834c2e06ca226884910f40168bfd817ce086e4
Mazy <twt_ios@163.com>
2016-11-09 15:59:46 +0800

    完善弹幕管理者对象

--- XMBarrageDemo/XMBarrageDemo/XMBarrage/XMBarrageManager.h
+++ XMBarrageDemo/XMBarrageDemo/XMBarrage/XMBarrageManager.h
@@ -7,7 +7,22 @@
 //
 
 #import <Foundation/Foundation.h>
+@class XMBarrageView;
 
 @interface XMBarrageManager : NSObject
+
+/** 开始生成弹幕视图block */
+@property(nonatomic,copy)void(^generateViewBlock)(XMBarrageView *view);
+
+/** 弹幕的数据源(存放字符型的数组) */
+@property(nonatomic,strong)NSMutableArray *dataSource;
+/** 弹幕的行数 */
+@property(nonatomic,assign)NSInteger barragesCount;
+
+// 弹幕开始执行
+- (void)start;
+
+// 弹幕停止执行
+- (void)stop;
 
 @end
--- XMBarrageDemo/XMBarrageDemo/XMBarrage/XMBarrageManager.m
+++ XMBarrageDemo/XMBarrageDemo/XMBarrage/XMBarrageManager.m
@@ -7,7 +7,180 @@
 //
 
 #import "XMBarrageManager.h"
+#import "XMBarrageView.h"
+
+@interface XMBarrageManager ()
+
+/** 弹幕使用过程中的数组变量 */
+@property(nonatomic,strong)NSMutableArray *barrageComments;
+
+/** 存储弹幕View的数组变量 */
+@property(nonatomic,strong)NSMutableArray *barrageViews;
+
+/** 存储弹幕弹道 */
+@property(nonatomic,strong)NSMutableArray *trajectorys;
+
+/** 监控状态 */
+@property(nonatomic,assign)BOOL stopAnimation;
+
+@end
 
 @implementation XMBarrageManager
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.stopAnimation = YES;
+    }
+    return self;
+}
+
+-(NSMutableArray *)dataSource {
+    if (_dataSource == nil) {
+        _dataSource = [NSMutableArray array];
+    }
+    return _dataSource;
+}
+
+-(NSMutableArray *)barrageViews {
+    if (_barrageViews == nil) {
+        _barrageViews = [NSMutableArray array];
+    }
+    return _barrageViews;
+}
+
+-(NSMutableArray *)barrageComments {
+    if (_barrageComments == nil) {
+        _barrageComments = [NSMutableArray array];
+    }
+    return _barrageComments;
+}
+
+// 弹幕开始执行
+- (void)start {
+    
+    if (self.stopAnimation) {
+        self.stopAnimation = NO;
+        [self.barrageComments removeAllObjects];
+        [self.barrageComments addObjectsFromArray:self.dataSource];
+        
+        [self initBarrageComments];
+    }
+}
+
+// 初始化弹幕,随机分配弹幕轨迹
+- (void)initBarrageComments {
+    
+    NSMutableArray *tempArray = [NSMutableArray array];
+    for (int i = 0; i < self.barragesCount; i++) {
+        [tempArray addObject:@(i)];
+    }
+    self.trajectorys = tempArray;
+    
+    for (int i = 0; i < self.barragesCount; i++) {
+        
+        if (self.barrageComments.count > 0) {
+            
+            // 通过随机数取出弹道的轨迹
+            NSInteger index = arc4random()%self.trajectorys.count;
+            
+            NSInteger trajectory = [[self.trajectorys objectAtIndex:index] integerValue];
+            
+            [self.trajectorys removeObjectAtIndex:index];
+            
+            // 从弹幕数组中,逐一取出弹幕数据
+            XMCommentModel *commentModel = [self.barrageComments firstObject];
+            [self.barrageComments removeObjectAtIndex:0];
+            
+            // 创建弹幕VIEW
+            [self createBarrageView:commentModel trajectory:trajectory];
+        }
+        
+    }
+}
+
+- (void)createBarrageView:(XMCommentModel *)commentModel trajectory:(NSInteger)trajectory {
+    
+    if (self.stopAnimation) {
+        return;
+    }
+    
+    XMBarrageView *barrageView = [[XMBarrageView alloc] initWithComment:commentModel];
+    barrageView.trajectory = trajectory;
+    [self.barrageViews addObject:barrageView];
+    
+    [barrageView startAnimation];
+    
+    __weak typeof(barrageView) weakBarrageView = barrageView;
+    __weak typeof(self) weakSelf = self;
+    barrageView.barrageStatusBlock = ^(barrageStatus status){
+        if (self.stopAnimation) {
+            return ;
+        }
+        switch (status) {
+            case Start:{
+                // 弹幕开始进入屏幕,将View添加到弹幕管理的变量中barragesViews
+                [weakSelf.barrageViews addObject:weakBarrageView];
+                break;
+            }
+            case Enter: {
+                // 弹幕完全进入屏幕中,判断是否还有其他内容,如果有,则再改弹道轨迹中创建一个弹幕
+                XMCommentModel *nextCommentModel = [self nextCommentModel];
+                if (nextCommentModel) {
+                    [weakSelf createBarrageView:nextCommentModel trajectory:trajectory];
+                }
+                break;
+            }
+            case End:{
+                // 当弹幕飞出屏幕后,从barragesView中删除,释放资源
+                if ([weakSelf.barrageViews containsObject:weakBarrageView]) {
+                    [weakBarrageView stopAnimation];
+                    [weakSelf.barrageViews removeObject:weakBarrageView];
+                }
+                
+                if (weakSelf.barrageViews.count==0) {
+                    // 说明屏幕上已经没有弹幕了,开始循环滚动
+                    self.stopAnimation = YES;
+                    
+                    [weakSelf start];
+                }
+                break;
+            }
+            default:
+                break;
+        }
+    };
+    
+    if (self.generateViewBlock) {
+        self.generateViewBlock(barrageView);
+    }
+}
+
+- (XMCommentModel *)nextCommentModel {
+    if (self.barrageComments.count == 0) {
+        return nil;
+    }
+    XMCommentModel *model = self.barrageComments.firstObject;
+    if (model) {
+        [self.barrageComments removeObject:model];
+    }
+    return model;
+}
+
+// 弹幕停止执行
+- (void)stop {
+    if (self.stopAnimation) {
+        return;
+    }
+    self.stopAnimation = YES;
+    [self.barrageViews enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        XMBarrageView *view = obj;
+        [view stopAnimation];
+        view = nil;
+    }];
+    [self.barrageViews removeAllObjects];
+}
+
 
 @end
