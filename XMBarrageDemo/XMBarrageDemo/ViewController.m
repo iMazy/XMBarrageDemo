e73484480437729bceb74dfe3b0b37a1687b4a15
Mazy <twt_ios@163.com>
2016-11-09 15:17:13 +0800

    初始化项目

---
+++ XMBarrageDemo/XMBarrageDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XMBarrageDemo
+//
+//  Created by TwtMac on 16/11/9.
+//  Copyright © 2016年 Mazy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
