76f56066664db9a1f88c511371b8bd7694d497a5
Mazy <twt_ios@163.com>
2016-11-09 15:48:24 +0800

    构建文件夹结构

---
+++ XMBarrageDemo/XMBarrageDemo/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMBarrageDemo
+//
+//  Created by TwtMac on 16/11/9.
+//  Copyright © 2016年 Mazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMBarrageDemo/XMBarrageDemo/ViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMBarrageDemo
+//
+//  Created by TwtMac on 16/11/9.
+//  Copyright © 2016年 Mazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
